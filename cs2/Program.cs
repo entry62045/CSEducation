85bf6d65039c8155f8de55e44bc2e2a99c3cd62d
entry62045 <solchan2007@gmail.com>
2021-02-19 18:08:06 +0900

    Add Files

---
+++ cs2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "안녕하세요. ";
+            string b = "62045입니다.";
+            string c = a + b;
+            int d = 2;
+            d *= 2;
+            float e = 1.2F;
+            bool f = true;
+            double g = 1.234;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.ReadKey();
+        }
+    }
+}
