bcf483c722078bedf7073aa61aed23410fe8ffe7
entry62045 <solchan2007@gmail.com>
2021-02-19 19:00:43 +0900

    강의 업데이트

--- cs1/Program.cs
+++ cs1/Program.cs
@@ -10,6 +10,9 @@ namespace cs1
     {
         static void Main(string[] args)
         {
+            Console.Write("안녕하세요. ");
+            Console.WriteLine("62045입니다.");
+            Console.WriteLine("C# 1강에서 만들어본 프로그램입니다.");
         }
     }
 }
--- cs2/Program.cs
+++ cs2/Program.cs
@@ -10,6 +10,20 @@ namespace cs2
     {
         static void Main(string[] args)
         {
+            string a = "안녕하세요. ";
+            string b = "62045입니다.";
+            string c = a + b;
+            int d = 2;
+            d *= 2;
+            float e = 1.2F;
+            bool f = true;
+            double g = 1.234;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.ReadKey();
         }
     }
 }
